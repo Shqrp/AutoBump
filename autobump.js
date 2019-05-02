9d29c86fe06d065070eb524f808c03851cfa32e2
Andrea Giurgola <Andrea.giurgo@gmail.com>
2019-05-02 11:07:20 +0200

    Removes extra operator and organizes the code.
    Removed an extra operator for if statement and organized the code in a better way.

--- autobump.js
+++ autobump.js
@@ -7,12 +7,11 @@ bot.login(TOKEN);
 bot.on("message", function(message)
 {
 
-    if (message.content === 'ab!start'){
+    if (message.content == 'ab!start'){
 
       message.channel.send("**Auto-bumping started! Next message in 20 minutes from now!**")  
 
-      var interval = setInterval (function ()       
-            
+      var interval = setInterval (function ()
             {
               message.channel.send("!bump")
             }, 1200000);
